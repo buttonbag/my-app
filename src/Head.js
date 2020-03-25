cb48c6cd6ee7cb9c2d29bd4dc7bc81778929aaa5
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-03-25 16:03:15 -0500

    example of a component within the same document

--- src/Head.js
+++ src/Head.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+// import Navigation from './head/Navigation';
+
+class Navigation extends Component {
+    render() {
+        return(
+            <ul className="navigation">
+                <li className="navItem">navItem</li>
+            </ul>
+        );
+    }
+}
 
 class Head extends Component {
     render() {
         return(
-            <div className="Head">Head</div>
+            <div className="Head">
+                Head
+                <Navigation />
+            </div>
         );
     }
 }
