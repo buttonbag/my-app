49e1127b6eb25868ab9b5133064065460c86890f
Louie Camacho <louiecamacho@Louies-MacBook-Pro-4.local>
2020-03-25 09:22:49 -0500

    components built

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Body extends Component {
+    render() {
+        return(
+            <div className="Body">Body</div>
+        );
+    }
+}
+
+export default Body;
