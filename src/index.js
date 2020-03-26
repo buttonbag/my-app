faf5b160b5537a39ab0a48af9e5757496bef13ff
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-03-26 13:20:13 -0500

    restarting, with all components in one location

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const Navigation = props => {
+  return(
+    <li>test</li>
+    );
+}
+
+class App extends Component {
+  render() {
+    
+    return(
+      <section>
+        <h1>Hello</h1>
+        <div className="Head">
+          <ul>
+            <Navigation />
+          </ul>
+        </div>
+        <div className="Body">Body</div>
+        <div className="Footer">Footer</div>
+      </section>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />, 
   document.getElementById('root')
 );
