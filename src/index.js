db47162d3ee009178997425384700bc509e3e4e5
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-04-02 17:59:45 -0500

    reset all, trying toggle function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+const Image = (props) => {
+  return (
+    <img src={props.src} />
+  )
+}
+
+class Button extends React.Component {
+  render(){
+    return(
+      <button>button</button>
+    );
+  }
+}
+
+class Root extends React.Component {
+  render(){
+    return(
+      <div className="container">
+        <Button />
+        <Image src="https://placekitten.com/1920/1080" />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <App />, 
+  <Root />, 
   document.getElementById('root')
 );
